d67ca1a56412472c1422be2392842e37cca39308
Toru45 <faiqradiansyah@gmail.com>
2025-05-27 12:08:52 +0700

    test: add unit tests for PasswordHash, AddUserUseCase, and UserRepository; improve RegisteredUser test formatting

--- src/Domains/users/entities/_test/RegisteredUser.test.js
+++ src/Domains/users/entities/_test/RegisteredUser.test.js
@@ -2,13 +2,13 @@ const RegisteredUser = require("../RegisteredUser");
 
 describe("a RegisteredUser entities", () => {
   it("should throw error when payload did not contain needed property", () => {
-    //Arrange
+    // Arrange
     const payload = {
       username: "dicoding",
       fullname: "Dicoding Indonesia",
     };
 
-    //Action and Assert
+    // Action and Assert
     expect(() => new RegisteredUser(payload)).toThrowError(
       "REGISTERED_USER.NOT_CONTAIN_NEEDED_PROPERTY"
     );
