d34bfcb754da8a0b3339525f85476ab565d0215d
Toru45 <faiqradiansyah@gmail.com>
2025-05-27 12:09:06 +0700

    feat: implement PasswordHash, AddUserUseCase, and UserRepository; refactor RegisterUser constructor for improved validation

---
+++ src/Domains/users/UserRepository.js
@@ -0,0 +1,14 @@
+/* UserRepository merupakan objek yang memiliki kumpulan fungsi, 
+di mana fungsi tersebut digunakan untuk berinteraksi dengan database (agen eksternal) dalam cakupan domain users.*/
+
+class UserRepository {
+  async addUser(registerUser) {
+    throw new Error("USER_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+
+  async verifyAvailableUsername(username) {
+    throw new Error("USER_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+}
+
+module.exports = UserRepository;
--- src/Domains/users/entities/RegisterUser.js
+++ src/Domains/users/entities/RegisterUser.js
@@ -1,15 +1,5 @@
 class RegisterUser {
-  constructor(payload) {
-    this._verifyPayload(payload);
-
-    const { username, password, fullname } = payload;
-
-    this.username = username;
-    this.password = password;
-    this.fullname = fullname;
-  }
-
-  _verifyPayload({ username, password, fullname }) {
+  constructor({ username, password, fullname }) {
     if (!username || !password || !fullname) {
       throw new Error("REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY");
     }
@@ -26,9 +16,13 @@ class RegisterUser {
       throw new Error("REGISTER_USER.USERNAME_LIMIT_CHAR");
     }
 
-    if (!username.match(/^[\w]+$/)) {
+    if (!username.match(/^[a-zA-Z1-9_]+$/)) {
       throw new Error("REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER");
     }
+
+    this.username = username;
+    this.password = password;
+    this.fullname = fullname;
   }
 }
 
